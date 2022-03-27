35ad2356c44b6ac49c78a85f8e86971d68af6156
Artyom <94921371+Phobos425@users.noreply.github.com>
2022-03-27 16:48:08 +0300

    Add files via upload

---
+++ templates/file.py
@@ -0,0 +1,12 @@
+import sqlalchemy
+from sqlalchemy import orm
+from db_session import SqlAlchemyBase
+
+
+class File(SqlAlchemyBase):
+    __tablename__ = 'files'
+
+    user = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    path = sqlalchemy.Column(sqlalchemy.String)
+
+    users = orm.relation("User", back_populates='file')
