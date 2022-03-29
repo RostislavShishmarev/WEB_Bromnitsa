dbd8d4bd2a05b345b21f972a291a314f7f2a03b7
Artyom <94921371+Phobos425@users.noreply.github.com>
2022-03-29 19:03:11 +0300

    Update file.py

--- templates/file.py
+++ templates/file.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from sqlalchemy import orm
 from db_session import SqlAlchemyBase
+import datetime
 
 
 class File(SqlAlchemyBase):
@@ -9,5 +10,7 @@ class File(SqlAlchemyBase):
     user = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     path = sqlalchemy.Column(sqlalchemy.String)
     name = sqlalchemy.Column(sqlalchemy.String)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
 
     users = orm.relation("User", back_populates='file')
