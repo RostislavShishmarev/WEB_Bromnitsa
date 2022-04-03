f695f69a5eaa87650881f13d23c83121491ecb74
Phobos425 <asuhomlin2007@yandex.ru>
2022-04-03 12:35:09 +0300

    pubs added

---
+++ data/users.py
@@ -0,0 +1,32 @@
+import sqlalchemy
+from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+from db_session import SqlAlchemyBase
+import datetime
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.String,
+                                nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              nullable=True,
+                              index=True,
+                              unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String,
+                                        nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+    photo = sqlalchemy.Column(sqlalchemy.String,
+                              nullable=True)
+
+    files = orm.relation("Publication", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
