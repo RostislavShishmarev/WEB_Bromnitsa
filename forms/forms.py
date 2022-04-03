4851b48ac53cb9459660236d6e8fa435759322e6
RostislavShishmarev <shishmarev.r@mail.ru>
2022-04-03 18:28:47 +0300

    Созданы страницы регистрации, авторизации и создания публикации

---
+++ forms/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, \
+    SubmitField, EmailField, BooleanField, FileField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Электронная почта', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    photo = FileField('Аватар',
+                      validators=[FileAllowed(['jpg', 'jpeg', 'png', 'svg',
+                                               'webp', 'gif', 'ico'],
+                                              'Неверный формат файла.'), ])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль',
+                                   validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Зарегистрироваться')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Электронная почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
 import os
+from flask import Flask, render_template, request
 from flask import send_from_directory
+from forms.forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super_Seсret_key_of_devEl0pers'
 SMALL, BIG = 'small', 'big'
+DESC = '''Мелодия из игры Отражение'''
 
 
 class CurrentSet:
@@ -13,13 +16,15 @@ class CurrentSet:
 class Autor:
     name = 'Моккий Кифович'
     photo = 'static/users/User_phoenix.jpg'
+    email = 'mokk@mail.ru'
 
 
 class Publication:
-    def __init__(self):
+    def __init__(self, filename, description, show_email=False):
         self.autor = Autor
-        self.filename = 'static/users/Exploding_block.png'
-        self.description = '''Мелодия из игры Отражение (GameReflection).&&Скачивайте на здоровье!'''
+        self.filename = 'static/users/' + filename
+        self.description = description
+        self.show_email = show_email
 
 
 @app.route('/favicon.ico')
@@ -64,8 +69,46 @@ def publications():
     nav = [{'href': '/', 'title': 'Главная'},
            {'href': '/login', 'title': 'Войти'}]
     return render_template('Publications.html', title='Публикации',
-                           navigation=nav, publications=[Publication(), Publication()],
+                           navigation=nav,
+                           publications=[Publication('Exploding_block.png',
+                                                     'Картинка взрывблока из \
+                                                     игры "Отражение"'),
+                                         Publication('play_fone.mp3',
+                                                     DESC, True)],
                            os=os)
+
+
+@app.route('/make_publication/<path:filename>',methods=['GET', 'POST'])
+def make_publication(filename):
+    filename = filename.replace('&', '/')
+    nav = [{'href': '/', 'title': 'Главная'},
+           {'href': '/publications', 'title': 'Публикации'},
+           {'href': '/cloud', 'title': 'Облако'},
+           {'href': '/settings', 'title': 'Настройки'},
+           {'href': '/logout', 'title': 'Выход'}]
+    return render_template('Publication_maker.html',
+                           title='Создать публикацию', navigation=nav, os=os,
+                           publication=Publication(filename, ''))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    nav = [{'href': '/login', 'title': 'Войти'},
+           {'href': '/', 'title': 'Главная'},
+           {'href': '/publications', 'title': 'Публикации'}]
+    return render_template('Form.html', title='Регистрация', navigation=nav,
+                           form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    nav = [{'href': '/register', 'title': 'Регистрация'},
+           {'href': '/', 'title': 'Главная'},
+           {'href': '/publications', 'title': 'Публикации'}]
+    return render_template('Form.html', title='Войти', navigation=nav,
+                           form=form)
 
 
 def sort_function(list_, cur_dir):
